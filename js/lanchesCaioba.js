36ab4eb8f2661e42b96ef33c1004e2df2563cd5f
Caio Monte Santo Barbosa <110570422+Cai0Sant0@users.noreply.github.com>
2024-02-17 19:42:31 -0300

    Tela lateral

--- js/lanchesCaioba.js
+++ js/lanchesCaioba.js
@@ -1,1 +1,17 @@
 // DESAFIO
+
+const btn_menu = document.querySelector("#menu");
+const abaLateral = document.querySelector(".abaLat");
+
+let abaBaerta = false;
+
+btn_menu.addEventListener("click",(evento)=>{
+   abaLateral.classList.add("abaLat_exibir");
+  }
+);
+
+
+
+
+
+
